ac0b97d9419ac05f44250ec8868e77627bbde669
Juan-Mateos <juan.mateos-garcia@nesta.org.uk>
2019-07-18 16:50:44 +0100

    Setup Nesta Data Science cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ai_analysis',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code for the analysis of AI research and development',
+    author='Juan Mateos-Garcia',
+    license='MIT',
+)
